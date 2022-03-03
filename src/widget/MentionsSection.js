cb57c0ebf9644551021ecf281714400f9e0e8453
Krzysztof Studniarek <krzysztofstudniarek@MacBook-Pro-Admin.local>
2022-03-03 16:41:34 +0100

    adding loader

--- src/widget/MentionsSection.js
+++ src/widget/MentionsSection.js
@@ -3,17 +3,23 @@ import styled from 'styled-components'
 import * as React from 'react'
 
 import {TwitterTweetEmbed} from 'react-twitter-embed'
+import TextContent from '../common/TextContent'
 
 const MentionsSectionContainter = styled.div`
     flex: 2;
-    padding: 20px;
+    padding: 10px;
 `
 const MentionsSection = (props) => {
+
+    let isLoaded = false;
+    
+    let onLoadFunc = () => {
+        isLoaded = true;
+    }
     return (
         <MentionsSectionContainter>
             {props.data.map((mention) => (
-                <TwitterTweetEmbed tweetId={mention}>
-
+                <TwitterTweetEmbed tweetId={mention} placeholder={<TextContent>Loading mnetion...</TextContent>}>
                 </TwitterTweetEmbed>
             ))}
         </MentionsSectionContainter>
