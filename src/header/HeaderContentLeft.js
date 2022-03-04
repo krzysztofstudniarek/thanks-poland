3e68ca497e087eb24bf7dc559e1181b30856dbca
Krzysztof Studniarek <krzysztofstudniarek@MacBook-Pro-Admin.local>
2022-03-04 12:25:25 +0100

    added constant counter

--- src/header/HeaderContentLeft.js
+++ src/header/HeaderContentLeft.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
 
-import {useState}  from 'react'
+import {useState, useEffect}  from 'react'
 
 const HeaderContentContainer = styled.div`
     flex: 1;
@@ -18,18 +18,27 @@ const HeaderContentContainer = styled.div`
 `
 
 const HeaderContentLeft = () => {
-    const calculateRefugeesNo = () => {
+    const updateRefugeesNo = () => {
         var baseNo = 550000;
         var now = moment(new Date());
         var start = moment("2022-03-03");
         var duration = moment.duration(now.diff(start));
         
-        return baseNo + Math.floor(duration.asMinutes())*34;
+        return baseNo + Math.floor(duration.asSeconds()*0.5);
     }
+
+    const [refugeesNo, setRefugeesNo] = useState(() => updateRefugeesNo());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+                setRefugeesNo(() => updateRefugeesNo());
+                console.log("UPDATED");
+            }, 2000);
+    }, []);
 
     return(
         <HeaderContentContainer>
-            Over <b>{calculateRefugeesNo().toLocaleString()}</b> citizens from 125 countries have already found safety in Poland.
+            Over <b>{refugeesNo.toLocaleString()}</b> citizens from 125 countries have already found safety in Poland.
         </HeaderContentContainer>
     )
 }
